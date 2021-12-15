50d0aab4385eb4572e9f82d7f385a1bc07235483
DavidNyan10 <DavidNyan211106@gmail.com>
2021-12-15 22:12:05 +0630

    snoostorm

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const snoowrap = require('snoowrap');
 const token = require('./token.json');
+const snoostorm = require('snoostorm');
 
 const r = new snoowrap(token);
 
@@ -36,93 +37,93 @@ if (!fs.existsSync(nodeModulesPath)) {
 
 function clean_string(raw_string) {
 	A = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567890".split('');
-    cleaned_string = raw_string.toLowerCase();
+	cleaned_string = raw_string.toLowerCase();
 	for(i = 0; i < cleaned_string.length; i++){
 		if (!A.includes(cleaned_string[i])){
 			cleaned_string = setCharAt(cleaned_string, i, " ");
 		}
 	}
-    cleaned_string = cleaned_string.replace(/\s\s+/g, ' ');
-    
-    return cleaned_string;
+	cleaned_string = cleaned_string.replace(/\s\s+/g, ' ');
+	
+	return cleaned_string;
 }
 
 function setCharAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substring(0,index) + chr + str.substring(index+1);
+	if(index > str.length-1) return str;
+	return str.substring(0,index) + chr + str.substring(index+1);
 }
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 
 class RedditBot{
-    constructor(filename){
-        this.response_list = [];
-        //if (!db.length){
+	constructor(filename){
+		this.response_list = [];
+		//if (!db.length){
 			this.response_list.push(new Promise((res) => {
-        		fs.createReadStream('response_list.csv')
-          			.pipe(csv(['phrase', 'reply1', 'reply2', 'reply3', 'reply4']))
-          			.on('data', (data) => {
-            			return(data);
-          			})
-      		}))
-        //  db['response_list'] = this.response_list
-        //} else{
-        //    console.log("Pulling from DB");
-        //    this.response_list = db['response_list'];
-        //}
-    }
-    async findMatch() {
-    	for(let i = 0; i < this.response_list.length; i++) {
-            console.log(i)
-            if (clean_string(comment.body).includes(this.response_list[i]['phrase'])){
-                if (this.cooled_down(i)){
-                    this.make_reply(i, comment);
-                }
-            }
-    	}
+				fs.createReadStream('response_list.csv')
+		  			.pipe(csv(['phrase', 'reply1', 'reply2', 'reply3', 'reply4']))
+		  			.on('data', (data) => {
+						return(data);
+		  			})
+	  		}))
+		//  db['response_list'] = this.response_list
+		//} else{
+		//    console.log("Pulling from DB");
+		//    this.response_list = db['response_list'];
+		//}
+	}
+	async findMatch() {
+		for(let i = 0; i < this.response_list.length; i++) {
+			console.log(i)
+			if (clean_string(comment.body).includes(this.response_list[i]['phrase'])){
+				if (this.cooled_down(i)){
+					this.make_reply(i, comment);
+				}
+			}
+		}
   	}
-    
-    cooled_down(i){
-        dictionary = this.response_list[i]
-        if (!dictionary.keys().includes('last_posted')){
-            // Means we have never posted on this phrase!
-            return True;
-        } else{
-            now = datetime.now();
-            duration = now - datetime.fromtimestamp(dictionary['last_posted']);
-            duration_seconds = duration.total_seconds();
-            hours = divmod(duration_seconds, 3600)[0];
-            if (hours >= 24){
-                return True;
-            } else{
-                console.log("Couldn't post " + dictionary['phrase'] + "Cool Down time: " + 24 - hours);
-            }
-        }
-        return False;
-    }
+	
+	cooled_down(i){
+		dictionary = this.response_list[i]
+		if (!dictionary.keys().includes('last_posted')){
+			// Means we have never posted on this phrase!
+			return True;
+		} else{
+			now = datetime.now();
+			duration = now - datetime.fromtimestamp(dictionary['last_posted']);
+			duration_seconds = duration.total_seconds();
+			hours = divmod(duration_seconds, 3600)[0];
+			if (hours >= 24){
+				return True;
+			} else{
+				console.log("Couldn't post " + dictionary['phrase'] + "Cool Down time: " + 24 - hours);
+			}
+		}
+		return False;
+	}
 
-    make_reply(i, comment){
-        dictionary = this.response_list[i];
-        try{
-            setTimeout(function(){
-                x = getRndInteger(1, 4);
-                comment.reply(dictionary['reply'+x]);
-                console.log(comment.body);
-                console.log(dictionary['phrase'])
-                console.log(dictionary['reply'+x])
-            }, 60 * 60 * 3);
-            // Might want to sleep after posting!
-        }catch(e){
-            console.log(e);
-        }
+	make_reply(i, comment){
+		dictionary = this.response_list[i];
+		try{
+			setTimeout(function(){
+				x = getRndInteger(1, 4);
+				comment.reply(dictionary['reply'+x]);
+				console.log(comment.body);
+				console.log(dictionary['phrase'])
+				console.log(dictionary['reply'+x])
+			}, 60 * 60 * 3);
+			// Might want to sleep after posting!
+		}catch(e){
+			console.log(e);
+		}
 
-        now = datetime.now();
-        this.response_list[i]['last_posted'] = now.timestamp();
-        //db['response_list'] = this.response_list
-    }
+		now = datetime.now();
+		this.response_list[i]['last_posted'] = now.timestamp();
+		//db['response_list'] = this.response_list
+	}
 }
 
 // Warning clears all your posted times!
@@ -130,8 +131,9 @@ class RedditBot{
 //db.clear()
 keep_alive();
 bot = new RedditBot("response_list.csv");
-/*for (i=0;i<subreddit.getNewComments();i++){
-    bot.find_match(comment);
-}*/
-r.getSubreddit('test').getNewComments().then(bot.findMatch);
-
+const comments = new snoostorm.CommentStream(r, {
+	subreddit: "test",
+	limit: 10,
+	pollTime: 2000,
+});
+comments.on("item", console.log);
